6ba25c6e98ad03538f669239d8ed6c5400995cec
aoles <andrzej.oles@gmail.com>
2026-02-26 12:21:46 +0100

    Fix remaining issues with example2

--- chapter2_3/example2.py
+++ chapter2_3/example2.py
@@ -8,9 +8,9 @@ def area_circ(r_in):
     :Returns: The area of the circle (float)."""
     if r_in < 0:
         raise ValueError("The radius must be >= 0.")
-    Kreis = np.pi * r_in**2
-    print(f"""The area of a circle with radius r = {r_in:3.2f}cm is A = {Kreis:4.2f}cm2.""")
-    return Kreis
+    circle = np.pi * r_in ** 2
+    print(f"""The area of a circle with radius r = {r_in:3.2f}cm is A = {circle:4.2f}cm2.""")
+    return circle
 
 
 if __name__ == "__main__":
